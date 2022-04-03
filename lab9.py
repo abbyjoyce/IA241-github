06f6a37df45d686dd44846911aab2efae1ee69be
EC2 Default User <ec2-user@ip-172-31-73-148.ec2.internal>
2022-04-03 19:17:15 +0000

    lab 9

--- lab9.py
+++ lab9.py
@@ -2,39 +2,42 @@
 lab 9 class
 '''
 
-class my_stat:
-    def cal_sigma(self,m,n):
+class my_stat():
+    
+    def sigma(m,n):
+        
         result = 0
+        
         for i in range(n,m+1):
-            result = result +i
+            result = result + i
             
         return result
         
-def cal_pi(self,n,m):
-    result = 1
-    for i in range(n,m+1):
-        result = result *1
-    return result
+    def pi(m,n):
+        
+        result = 1
+        
+        for i in range(n,m+1):
+            result = result * 1
+       
+        return result
     
-def cal_f(self,m):
-    if m ==0:
-        return 1
-    else:
-        return m * self.cal_f(m-1)
+    def cal_f(self,m):
+        if m ==0:
+            return 1
+        else:
+            return m * self.cal_f(m-1)
+            
+        return result
 
-def cal_p(self,m,n):
-    if m ==0:
-        return 1
-    else:
-        return m * self.cal_f
-def cal_p(self,m,n):
-    return self.cal_f(m)/self.cal_f(m-n)
+    def cal_p(self,m,n):
+        return self.cal_f(m)/self.cal_f(m-n)
 
 #3.2
 
 my_cal = my_stat()
 
-print(my_cal.cal_sigma(5,3))
-print(my_cal.cal_pi(5,3))
-print(my_cal.calf(5))
+print(my_stat.sigma(5,3))
+print(my_stat.pi(5,3))
+print(my_cal.cal_f(5))
 print(my_cal.cal_p(5,2))
